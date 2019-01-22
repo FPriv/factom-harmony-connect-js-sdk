0108a1d4aa78e17f6d6930b7e4cf00d97660c493
bephahoai <hoangnguyen08s@gmail.com>
2019-01-22 20:23:56 +0700

    Identity

--- tests/identity.test.js
+++ tests/identity.test.js
@@ -30,16 +30,16 @@ describe('IDENTITY Test', () => {
         },
       });
     });
-    it('should return error message when name is missing', () => {
+    it('should return error message when name is missing', async () => {
       try {
-        identity.createAnIdentity();
+        await identity.createAnIdentity();
       } catch (error) {
         expect(error).toEqual(new Error('name is required.'));
       }
     });
-    it('should return error message when keys is missing', () => {
+    it('should return error message when keys is missing', async () => {
       try {
-        identity.createAnIdentity(['123']);
+        await identity.createAnIdentity(['123']);
       } catch (error) {
         expect(error).toEqual(new Error('keys is required.'));
       }
@@ -70,6 +70,83 @@ describe('IDENTITY Test', () => {
       axios.mockImplementationOnce(() => Promise.resolve(resp));
       const response = await identity.createAnIdentity(['1'], ['1'], '', ['factom', 'replicated']);
       expect(axios).toHaveBeenCalledWith('https://apicast.io/identities', { data: data, headers: { 'Content-Type': 'application/json', app_id: '123456', app_key: '123456789' }, method: 'POST' });
+      expect(response).toEqual(resp.data);
+    });
+  });
+  describe('Get an Identity', () => {
+    let identity;
+    beforeAll(() => {
+      identity = new IDENTITY({
+        baseURL: 'https://apicast.io',
+        accessToken: {
+          app_id: '123456',
+          app_key: '123456789',
+        },
+      });
+    });
+    it('should return error message when chain id is missing', async () => {
+      try {
+        await identity.getAnIdentity();
+      } catch (error) {
+        expect(error).toEqual(new Error('chain id is required.'));
+      }
+    });
+    it('should get an Identity successfully.', async () => {
+      const resp = {
+        status: 200,
+        data: {
+          version: 1,
+          stage: 'anchored',
+          created_height: 118460,
+          chain_id: '171e5851451ce6f2d9730c1537da4375feb442870d835c54a1bca8ffa7e2bda7',
+          name: ['RU1QTE9ZRUU=', 'QUJDMTIz'],
+        },
+      };
+
+      axios.mockImplementationOnce(() => Promise.resolve(resp));
+      const response = await identity.getAnIdentity('123456');
+      expect(axios).toHaveBeenCalledWith('https://apicast.io/identities/123456', { data: '', headers: { 'Content-Type': 'application/json', app_id: '123456', app_key: '123456789' }, method: 'GET' });
+      expect(response).toEqual(resp.data);
+    });
+  });
+  describe('Get all Identity Keys', () => {
+    let identity;
+    beforeAll(() => {
+      identity = new IDENTITY({
+        baseURL: 'https://apicast.io',
+        accessToken: {
+          app_id: '123456',
+          app_key: '123456789',
+        },
+      });
+    });
+    it('should return error message when chain id is missing', async () => {
+      try {
+        await identity.getAllIdentityKeys();
+      } catch (error) {
+        expect(error).toEqual(new Error('chain id is required.'));
+      }
+    });
+    it('should get all Identity Keys successfully.', async () => {
+      const resp = {
+        status: 200,
+        data: [
+          {
+            key: 'idpub2Cktw6EgcBVMHMXmfcCyTHndcFvG7fJKyBpy3sTYcdTmdTuKya',
+            activated_height: 118460,
+            retired_height: null,
+          },
+          {
+            key: 'idpub2JegfdBQBnqbXGKMMD89v8N81e4DpvERHWTJp6zvWaoAVi8Jnj',
+            activated_height: 118460,
+            retired_height: null,
+          },
+        ],
+      };
+
+      axios.mockImplementationOnce(() => Promise.resolve(resp));
+      const response = await identity.getAllIdentityKeys('123456');
+      expect(axios).toHaveBeenCalledWith('https://apicast.io/identities/123456/keys', { data: '', headers: { 'Content-Type': 'application/json', app_id: '123456', app_key: '123456789' }, method: 'GET' });
       expect(response).toEqual(resp.data);
     });
   });
